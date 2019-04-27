76150a8f46eceed6bd5538e012ffbc5263fc8fa3
trungkien110298 <44560514+trungkien110298@users.noreply.github.com>
2019-04-27 15:59:57 +0700

    27/04/2019

--- app/src/main/java/vn/edu/hust/soict/kien_hoang/planer/Week.java
+++ app/src/main/java/vn/edu/hust/soict/kien_hoang/planer/Week.java
@@ -3,4 +3,11 @@ package vn.edu.hust.soict.kien_hoang.planer;
 public class Week {
     private Day[] days = new Day[7];
 
+    public Day[] getDays() {
+        return days;
+    }
+
+    public void setDays(Day[] days) {
+        this.days = days;
+    }
 }
