c8d26fca0c3f272d69b076f738bf1af856a1fdb6
trungkien110298 <44560514+trungkien110298@users.noreply.github.com>
2019-05-23 13:12:12 +0700

    Login SignUp Fixed

--- app/src/main/java/vn/edu/hust/soict/kien_hoang/planer/UserHelper.java
+++ app/src/main/java/vn/edu/hust/soict/kien_hoang/planer/UserHelper.java
@@ -4,46 +4,57 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class UserHelper extends SQLiteOpenHelper {
 
     private static final String DATABASE_NAME = "planner.db";
     private static final int SCHEMA_VERSION = 1;
-    public UserHelper(Context context){
+
+    public UserHelper(Context context) {
         super(context, DATABASE_NAME, null, SCHEMA_VERSION);
     }
 
     @Override
-    public void onCreate(SQLiteDatabase db){
+    public void onCreate(SQLiteDatabase db) {
         db.execSQL("CREATE TABLE users (_id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT);");
     }
+
     @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         // no-op, since will not be called until 2nd schema
         // version exists
     }
 
     public Cursor getAll() {
-        return(getReadableDatabase()
-                .rawQuery("SELECT _id, username, password FROM users ORDER BY _id",null));
+        return (getReadableDatabase()
+                .rawQuery("SELECT _id, username, password FROM users ORDER BY _id", null));
     }
 
-    public Cursor getByUsername(String username){
-        return (getReadableDatabase()
-                .rawQuery("SELECT _id, username, password FROM users WHERE username =" + username,null));
+    public Cursor getByUsername(String username) {
+        Cursor c = null;
+        try {
+            c = getReadableDatabase()
+                    .rawQuery("SELECT _id, username, password FROM users WHERE username = ?", new String[]{username});
+        } catch (SQLiteException e) {
+            return null;
+        }
+        return c;
     }
 
     public void insert(String username, String password) {
-        ContentValues cv=new ContentValues();
+        ContentValues cv = new ContentValues();
         cv.put("username", username);
         cv.put("password", password);
         getWritableDatabase().insert("users", "username", cv);
     }
+
     public String getUsername(Cursor c) {
-        return(c.getString(1));
+        return (c.getString(1));
     }
+
     public String getPassword(Cursor c) {
-        return(c.getString(2));
+        return (c.getString(2));
     }
 }
