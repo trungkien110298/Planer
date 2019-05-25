f5767b65d6ea441f94af3e68ca30cc3ecdb7892b
trungkien110298 <44560514+trungkien110298@users.noreply.github.com>
2019-05-25 16:19:31 +0700

    Some change

--- app/src/main/java/vn/edu/hust/soict/kien_hoang/planer/CalendarActivity.java
+++ app/src/main/java/vn/edu/hust/soict/kien_hoang/planer/CalendarActivity.java
@@ -2,12 +2,15 @@ package vn.edu.hust.soict.kien_hoang.planer;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class CalendarActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.calendar);
-
+        Log.d("Check", "Ok");
+        Toast.makeText(getBaseContext(), "Hello", Toast.LENGTH_LONG).show();
     }
 }
--- app/src/main/java/vn/edu/hust/soict/kien_hoang/planer/NowActivity.java
+++ app/src/main/java/vn/edu/hust/soict/kien_hoang/planer/NowActivity.java
@@ -8,18 +8,15 @@ import android.view.View;
 import android.widget.Button;
 
 public class NowActivity extends Activity {
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.now);
-        Log.d("Check", "OK");
+
         Button addTaskButton = (Button) findViewById(R.id.editCurrentTaskBtn);
         addTaskButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.d("Check", "OK");
                 Intent newTaskIntent = new Intent(NowActivity.this, NewTaskActivity.class);
                 startActivity(newTaskIntent);
 
