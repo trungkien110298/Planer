f336208a4b325518e7ad331c5591e7a5a49132c9
trungkien110298 <44560514+trungkien110298@users.noreply.github.com>
2019-05-26 22:08:19 +0700

    Update NowActivity.java

--- app/src/main/java/vn/edu/hust/soict/kien_hoang/planer/NowActivity.java
+++ app/src/main/java/vn/edu/hust/soict/kien_hoang/planer/NowActivity.java
@@ -1,15 +1,8 @@
 package vn.edu.hust.soict.kien_hoang.planer;
 
 import android.app.Activity;
-import android.app.job.JobInfo;
-import android.app.job.JobParameters;
-import android.app.job.JobScheduler;
-import android.app.job.JobService;
-import android.content.ComponentName;
-import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
-import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -17,6 +10,7 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import java.sql.Time;
+import java.util.Calendar;
 
 public class NowActivity extends Activity {
     private TextView taskName;
@@ -67,19 +61,24 @@ public class NowActivity extends Activity {
             public void run() {
                 try {
                     Log.d("Check", "ok in count");
+                    long lTimeLeft, lCurrentTime, lFinishTime;
                     if (task == null) {
-                        Cursor c = taskHelper.getAll();
-                        c.moveToFirst();
-                        do {
-
-                        } while (c.moveToNext());
+                        task = getNewTask();
                     } else {
-
+                        lCurrentTime = Calendar.getInstance().getTimeInMillis();
+                        lFinishTime = Time.valueOf(task.getFinishTime()).getTime();
+                        lTimeLeft = lFinishTime - lCurrentTime;
+                        if(lTimeLeft < 0)
+                            task = getNewTask();
                     }
                 } catch (Exception e) {
                 }
             }
         });
+    }
+
+    private Task getNewTask(){
+
     }
 
     /**
